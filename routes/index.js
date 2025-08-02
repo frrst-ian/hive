34f128dff68ef5c19de70d9f43fd4bafe82c38cf
frrst-ian <frrstxd@proton.me>
2025-08-02 09:21:03 +0800

    fix: remove redundant get route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,5 @@ const indexRouter = Router();
 const indexController = require("../controllers/indexController");
 
 indexRouter.get("/", indexController.getIndex);
-indexRouter.get("/", indexController.getAllMessagesHandler);
 
 module.exports = indexRouter;
