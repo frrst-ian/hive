f913cb0461b3edf55c2b15f7c2fc027265d81f47
frrst-ian <frrstxd@proton.me>
2025-08-02 13:50:02 +0800

    feat: add asyn functions for rendering admin form and handling password input

---
+++ controllers/adminController.js
@@ -0,0 +1,34 @@
+const db = require("../db/queries");
+const adminPassword = process.env.ADMIN_PASSWORD;
+
+async function renderAdminForm(req, res) {
+    try {
+        res.render("admin-form");
+    } catch (error) {
+        res.status(500).send("Internal Server Error");
+    }
+}
+
+async function adminHandler(res, req, next) {
+    try {
+        const userId = req.user.id;
+        const password = req.body.password;
+
+        if (password !== adminPassword) {
+            return res.render("admin-form", { error: "Invalid Password" })
+        }
+
+        const user = await db.getUserById(userId);
+
+        if (user.membership_status = 'admin') {
+            res.redirect("/");
+        }
+
+        await db.updateMembership(userId);
+        res.redirect("/")
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = { renderAdminForm, adminHandler };
