6880dc7e7efbec19adc6512d9dc618c13a5e2d86
frrst-ian <frrstxd@proton.me>
2025-08-02 09:20:02 +0800

    refactor: pass the messages to the views

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,21 +1,13 @@
 const db = require("../db/queries");
 
 async function getIndex(req, res) {
-    res.render("index", { user: req.user })
-}
-
-async function getAllMessagesHandler(req, res) {
     try {
         const messages = await db.getAllMessages();
 
-        if (!messages) {
-            res.render("index", { messages, edgeCase: "No messages exists." });
-        }
-
-        res.render("index");
+        res.render("index", { user: req.user, messages });
     } catch (error) {
         res.status(505).send("No messages exists");
     }
 }
 
-module.exports = { getIndex, getAllMessagesHandler };
+module.exports = { getIndex };
