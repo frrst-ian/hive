c0f283a3c2967f90dc8fb61d566c40f5ab8fa0c2
frrst-ian <frrstxd@proton.me>
2025-08-02 10:46:35 +0800

    fix: change to status(500) which indicates server error

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,10 +3,9 @@ const db = require("../db/queries");
 async function getIndex(req, res) {
     try {
         const messages = await db.getAllMessages();
-
         res.render("index", { user: req.user, messages });
     } catch (error) {
-        res.status(505).send("No messages exists");
+        res.status(500).send("Server Error");
     }
 }
 
