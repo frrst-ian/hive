0a4f65a5ef99e5efb04395c1be2fe38613263cfe
frrst-ian <frrstxd@proton.me>
2025-08-03 09:29:13 +0800

    chore: create a dummy author name for non-members

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,7 @@ const db = require("../db/queries");
 async function getIndex(req, res) {
     try {
         const messages = await db.getAllMessages();
-        res.render("index", { user: req.user, messages });
+        res.render("index", { user: req.user, messages, author: "Say my name" });
     } catch (error) {
         res.status(500).send("Server Error");
     }
