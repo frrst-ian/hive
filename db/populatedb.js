93bf392410c69f87153d0570763f660e287b8079
frrst-ian <frrstxd@proton.me>
2025-08-06 12:26:34 +0800

    fix: set the default membership_status to basic

--- db/populatedb.js
+++ db/populatedb.js
@@ -13,7 +13,7 @@ first_name VARCHAR(50) NOT NULL,
 last_name VARCHAR(50),
 email VARCHAR(50) NOT NULL UNIQUE,
 password VARCHAR(255) NOT NUll,
-membership_status VARCHAR(50) DEFAULT 'active',
+membership_status VARCHAR(50) DEFAULT 'basic',
 created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
