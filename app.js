fd28e5ca5f1b5a872140d33787be0e10699bf635
frrst-ian <frrstxd@proton.me>
2025-08-06 11:59:54 +0800

    fix: allow SSL

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   store: new pgSession({
-    conString: process.env.DB_URL,
+    pool: pool,
     createTableIfMissing: true,
     ssl:true
   })
