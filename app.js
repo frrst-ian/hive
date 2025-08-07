5f5aa94dd29a19572073933ba8e13d3a31c8646b
frrst-ian <frrstxd@proton.me>
2025-08-07 10:02:43 +0800

    fix: use env variable for session

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(session({
-  secret: SESSION_SECRET,
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: false,
   store: new pgSession({
